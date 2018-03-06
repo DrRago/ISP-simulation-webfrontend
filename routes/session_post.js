cf07f8d419fd6d791b218de4b77ae48e5ffea8da
Leonhard Gahr <leonhard.gahr@gmail.com>
2018-03-06 19:24:20 +0100

    change mysql login query

--- routes/session_post.js
+++ routes/session_post.js
@@ -9,7 +9,7 @@ exports.login = (request, response) => {
   // Get MD5 Hashed Password
   const password_md5 = md5(password);
 
-  constants.CONNECTION.query("SELECT * FROM radcheck WHERE username=" + username + " AND value=" + password_md5 + ";", (error, result) => {
+  constants.CONNECTION.query("SELECT * FROM radcheck WHERE username=`" + username + "` AND value=`" + password_md5 + "`;", (error, result) => {
     if (error) throw error;
     console.log("Result: " + result);
   });
